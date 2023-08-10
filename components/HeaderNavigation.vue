5991142ffd4a0044c6ab6de89383825c27868cf0
Leo <leojunebedeo@gmail.com>
2023-08-10 15:49:45 +0800

    Move scss to style tag

--- components/HeaderNavigation.vue
+++ components/HeaderNavigation.vue
@@ -23,17 +23,18 @@
                     <a href="#" class="sign-up-btn flex items-center justify-center">Sign Up</a>
                 </div>
             </nav>
-            <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+            <Dialog as="div" class="lg:hidden header-navigation" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
                 <div class="fixed inset-0 z-50" />
                 <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                 <div class="flex items-center justify-between">
-                    <a href="#" class="-m-1.5 p-1.5">
-                    <span class="sr-only">Your Company</span>
-                    <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                    <a href="#" class="-m-1.5 p-1.5 company-logo">
+                        <span class="sr-only">Biccas</span>
+                        <!-- <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" /> -->
+                        Biccas
                     </a>
                     <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
-                    <span class="sr-only">Close menu</span>
-                    <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                        <span class="sr-only">Close menu</span>
+                        <XMarkIcon class="h-6 w-6" aria-hidden="true" />
                     </button>
                 </div>
                 <div class="mt-6 flow-root">
@@ -41,8 +42,11 @@
                         <div class="space-y-2 py-6">
                             <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 header-navigation-link" :class="{ active: item.isActive }">{{ item.name }}</a>
                         </div>
-                        <div class="py-6">
+                        <div class="pt-6">
                             <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
+                        </div>
+                        <div class="pb-6">
+                            <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Sign up</a>
                         </div>
                     </div>
                 </div>
@@ -67,3 +71,59 @@ const navigation = [
 
 const mobileMenuOpen = ref(false)
 </script>
+
+<style lang="scss" scoped>
+    .header-navigation {
+        .company-logo {
+            color: #49a582;
+            font-size: 48px;
+            font-family: Inter;
+            font-style: normal;
+            font-weight: 600;
+            line-height: 30px;
+        }
+
+        .header-navigation-link {
+            color: #A6A6A6;
+            font-size: 16px;
+            font-family: Inter;
+            font-style: normal;
+            font-weight: 500;
+            line-height: normal;
+
+            &.active {
+                color: #000;
+                font-size: 18px;            
+            }
+        }
+
+        .login-btn {
+            color: #A6A6A6;
+            font-size: 16px;
+            font-family: Inter;
+            font-style: normal;
+            font-weight: 500;
+            line-height: normal;
+
+            width: 80px;
+            height: 40px;
+            flex-shrink: 0;
+        }
+
+        .sign-up-btn {
+            color: #F8F8FA;
+            font-size: 16px;
+            font-family: Inter;
+            font-style: normal;
+            font-weight: 500;
+            line-height: normal;
+            
+            width: 80px;
+            height: 40px;
+            flex-shrink: 0;
+
+            border-radius: 10px;
+            background: #49a582;
+        }
+    }
+</style>
--- components/Hero.vue
+++ components/Hero.vue
@@ -4,15 +4,15 @@
         <div class="upper-right-circle z-40 hidden lg:block"></div>
         <div class="middle-circle z-40 hidden lg:block"></div>
         <div class="bottom-right-circle z-40 hidden lg:block"></div>
-        <div class="relative grid px-6 lg:px-8 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 max-w-screen-xl z-50">
-            <div class="mr-auto place-self-center col-span-12 lg:col-span-6 order-2 lg:order-1">
-                <h1 class="lg:max-w-2xl mb-7 text-4xl md:text-5xl xl:text-7xl primary-text leading-10 text-center md:text-start">We’re here to Increase your Productivity</h1>
-                <div class="w-full lg:max-w-lg">
+        <div class="relative grid px-6 lg:px-8 py-8 mx-auto gap-8 lg:gap-0 lg:py-16 lg:grid-cols-2 max-w-screen-xl z-50">
+            <div class="mr-auto place-self-center order-2 lg:order-1">
+                <h1 class="lg:max-w-2xl mb-7 text-4xl md:text-5xl xl:text-7xl primary-text leading-10 text-center lg:text-start">We’re here to Increase your Productivity</h1>
+                <div class="w-full lg:max-w-lg hidden lg:block">
                     <svg height="34" viewBox="0 0 487 34" fill="none" xmlns="http://www.w3.org/2000/svg" class="mb-12 w-full">
                         <path d="M4 30C73.6307 10.3798 266.914 -17.0885 483 30" stroke="#54BD95" stroke-width="8" stroke-linecap="round"/>
                     </svg>
                 </div>
-                <p class="lg:max-w-lg mb-6 lg:mb-8 secondary-text">Let's make your work more organize and easily using the Taskio Dashboard with many of the latest featuresin managing work every day.</p>
+                <p class="lg:max-w-lg mb-6 lg:mb-8 secondary-text text-center lg:text-start">Let's make your work more organize and easily using the Taskio Dashboard with many of the latest featuresin managing work every day.</p>
                 <div class="flex justify-center lg:justify-start flex-col lg:flex-row">
                     <a href="#" class="inline-flex items-center justify-center px-7 mr-0 lg:mr-3 rounded-full bg-primary-color focus:ring-4 focus:ring-primary-300 h-14 free-trial-text mb-3 lg:mb-0">
                         Try free trial
@@ -26,7 +26,7 @@
                     </a> 
                 </div>
             </div>
-            <div class="lg:mt-0 col-span-12 lg:col-span-6 order-1 lg:order-2">
+            <div class="lg:mt-0 order-1 lg:order-2 mx-auto">
                 <img src="/images/Group-138.png" alt="mockup">
             </div>                
         </div>
@@ -38,3 +38,94 @@
         
     }
 </script>
+
+<style lang="scss" scoped>
+    .hero-section {
+
+        .primary-text {
+            color: #191A15;
+            font-family: Inter;
+            font-style: normal;
+            font-weight: 700;
+        }
+
+        .secondary-text {
+            color: #000;
+            font-size: 18px;
+            font-family: Inter;
+            font-style: normal;
+            font-weight: 500;
+            line-height: 30px;
+        }
+
+        .free-trial-text {
+            color: #FFF;
+            font-size: 18px;
+            font-family: Inter;
+            font-style: normal;
+            font-weight: 500;
+            line-height: normal;        
+        }
+
+        .view-demo-text {
+            color: #191A15;
+            font-size: 18px;
+            font-family: Inter;
+            font-style: normal;
+            font-weight: 500;
+            line-height: normal;        
+        }
+
+        .upper-left-circle {
+            position: absolute;
+            top: -437px;
+            left: -317px;
+
+            width: 634px;
+            height: 634px;
+
+            border-radius: 634px;
+            background: #52BDAA;
+            filter: blur(500px);
+        }
+
+        .upper-right-circle {
+            position: absolute;
+            top: -437px;
+            right: 225px;
+
+            width: 634px;
+            height: 634px;
+
+            border-radius: 634px;
+            background: #FFD6D6;
+            filter: blur(500px);
+        }
+
+        .middle-circle {
+            position: absolute;
+            top: calc(50% - 317px);
+            left: calc(50% - 317px);
+
+            width: 634px;
+            height: 634px;
+
+            border-radius: 634px;
+            background: #FFF;
+            filter: blur(500px);
+        }
+
+        .bottom-right-circle {
+            position: absolute;
+            top: 270px;
+            right: -317px;
+
+            width: 634px;
+            height: 634px;
+
+            border-radius: 634px;
+            background: #54BE96;
+            filter: blur(500px);
+        }
+        }
+</style>
